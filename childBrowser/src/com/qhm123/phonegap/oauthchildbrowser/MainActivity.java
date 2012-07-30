9ebf506a4d2c7db38336279584164be8788d1b4f
qhm123 <qhm123@gmail.com>
2012-07-30 20:44:19 +0800

    add child browser oauth demo

---
+++ childBrowser/src/com/qhm123/phonegap/oauthchildbrowser/MainActivity.java
@@ -0,0 +1,17 @@
+package com.qhm123.phonegap.oauthchildbrowser;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+}
